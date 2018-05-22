822f30ac8ee3305d3cb6aeb13edbb6c259a7cc3b
haozhen <624536203@qq.com>
2018-05-22 18:02:45 +0800

    基本完成

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ $(function(){
     var i = 0;
     lunBo();
     function lunBo() {
+        if(i==0){
+            $(".lun").css({"background-color":"white"});
+        }else if(i==1){
+
+        }else if(i==2){
+            $(".lun").css({"background-color":"rgba(255, 255, 255, 0.877)"});
+        }else if(i==3){
+            $(".lun").css({"background-color":"#efeeee"});
+        }
+        //隐藏img
         $(".lun >img").hide();
+        //隐藏标题
         $(".div").hide();
+        //动画效果
         $(".div").eq(i).show("fold",1000);
         $(".lun >img").eq(i).show("drop",500);
+        
         $(".second > ul > li").eq(i).animate({
             width:"50px"
         },8000);
